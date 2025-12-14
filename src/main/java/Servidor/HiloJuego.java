10fc32d070f706d8f58f15898cd3d32be566321e
unknown <you@example.com>
2025-12-14 02:24:44 +0100

    hilo hecho

--- src/main/java/Servidor/HiloJuego.java
+++ src/main/java/Servidor/HiloJuego.java
@@ -1,14 +1,46 @@
 package Servidor;
 
+import java.io.*;
 import java.net.Socket;
+import java.util.Random;
 
 public class HiloJuego implements Runnable{
     private Socket socket;
+    BufferedReader in;
+    PrintWriter out;
+    int numeroSecreto=(int) (Math.random()*100)+1;
+
 public HiloJuego(Socket socket){
     this.socket=socket;
+    this.numeroSecreto=(int) (Math.random()*100)+1;
+    try {
+in= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+out= new PrintWriter(socket.getOutputStream(),true);
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
 }
+
     @Override
     public void run() {
+
+    try {
+        String mensaje;
+        while ((mensaje = in.readLine()) != null){
+            if (Integer.parseInt(mensaje)>numeroSecreto){
+                out.println("el numero es mas pequeño");
+            } else if (Integer.parseInt(mensaje)<numeroSecreto) {
+                out.println("el numero es mas grande");
+            }else {
+                out.println("correcto");
+                break;
+            }
+        }
+
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+
 
     }
 }
